0dc885c9d8fdcb740c577b42dca54b16d1586348
Eric Yang <jie9481@gmail.com>
2025-06-21 03:13:33 +0800

    Initial Commit

---
+++ Launchpad_Back/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Launchpad_Back
+//
+//  Created by Eric Yang on 6/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Launchpad_Back/Launchpad_BackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Launchpad_BackApp.swift
+//  Launchpad_Back
+//
+//  Created by Eric Yang on 6/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Launchpad_BackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
